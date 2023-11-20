fbf4ac78f87c8d1d64d04af1b4df4c8ff8755f18
jwwinship <75297376+jwwinship@users.noreply.github.com>
2023-11-19 22:57:34 -0500

    Add files via upload

---
+++ rtl/RNG.v
@@ -0,0 +1,9 @@
+module RNG(input  wire clk,
+              input  wire reset,
+              input  wire start,
+              output wire [31:0] result,
+	      output wire done);
+   
+   
+   
+endmodule
