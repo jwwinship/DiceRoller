6d5c669cb9cacae20b543d4aabde120d8ec32737
Winship <jwwinship@arc-schaumont-class-vm.wpi.edu>
2023-12-06 16:07:54 -0500

    SIPO functions with one bit per cycle

--- rtl/SIPO.v
+++ rtl/SIPO.v
@@ -1,10 +1,11 @@
+`timescale 1ns/1ps
 /* Serial In, Parallel Out module
 	Recieves one bit per 'cycles_per_bit' cycles, storing these bits in a shift register
 	
 	TODO: Implement state machine?
 */
 
-module SIPO #(parameter cycles_per_bit = 10) 
+module SIPO #(parameter cycles_per_bit = 0) 
 	(
 		input clk,
 		input reset_n,
@@ -41,8 +42,35 @@ module SIPO #(parameter cycles_per_bit = 10)
 		end
 		else begin
 			r_current_state <= r_current_state;
-			r_cycle_ctr++;	
+			//r_cycle_ctr <= r_cycle_ctr + 1;	
 		end
+		
+		case(r_next_state)
+			s_IDLE: begin
+				r_cycle_ctr <= 8'b0; //Reset cycle counter
+				r_valid <= 0;
+				r_data_temp <= 0;
+				r_bit_ctr <= 0;
+			end
+			
+			s_START: begin
+				r_valid <= 0;
+			end
+			
+			s_SHIFT: begin
+				//if (r_cycle_ctr == cycles_per_bit) begin //If we've gone through enough cycles for data to be available, execute shift
+					//r_cycle_ctr <= 8'b0; //Reset cycle counter
+					r_data_temp <= {i_data_in, r_data_temp[6:1]}; //Shift in new value
+					r_bit_ctr <= r_bit_ctr + 1; //Add 1 to bit counter
+			end
+			
+			s_DONE: begin
+				r_valid <= 1;
+			end
+			
+			default: r_data_temp <= 7'b1110111;
+			
+		endcase
 	end
 	
 	assign o_valid = r_valid;
@@ -52,31 +80,45 @@ module SIPO #(parameter cycles_per_bit = 10)
 	//Combinational logic
 	always @(*) 
 	begin
-		case (r_current_state) begin
+		case(r_current_state)
 			s_IDLE: begin
-				r_cycle_ctr <= 8'b0; //Reset cycle counter
-				r_valid <= 0;
-				r_data_temp <= 0;
-
 				if (i_start) begin
-					r_next_state <= s_START
+					r_next_state <= s_SHIFT;
+				end
+				
+				else 
+				begin
+					r_next_state <= s_IDLE;
 				end
 			end
 			
-			s_
+			s_START: begin
+				r_next_state <= s_SHIFT; //A one cycle delay for start bit
+			end
+			
+			s_SHIFT: begin
+				if (r_cycle_ctr == cycles_per_bit) begin //If we've gone through enough cycles for data to be available, execute shift
+					r_cycle_ctr <= 8'b0; //Reset cycle counter
+
+					if (r_bit_ctr == 3'd7) 
+					begin
+						//r_valid <= 1; //If we have all bits in the register, set the valid flag
+						r_next_state <= s_DONE;
+					end
+					
+					else r_next_state <= s_SHIFT;
+				end
+				
+				else r_cycle_ctr <= r_cycle_ctr + 1;
+			end
+			
+			s_DONE: begin
+				r_next_state <= s_IDLE;
+			end
+			
+			default: r_next_state <= r_current_state;
+			
 		endcase
-		
-		else if (r_cycle_ctr == cycles_per_bit-1) begin //If we've gone through enough cycles for data to be available, execute shift
-			r_cycle_ctr <= 8'b0; //Reset cycle counter
-			r_data_temp <= {i_data_in, r_data_temp[5:0]} //Shift in new value
-			r_bit_ctr <= r_bit_ctr + 1; //Add 1 to bit counter
-			if (r_bit_ctr == 3'd7) 
-			begin
-				r_valid <= 1; //If we have all bits in the register, set the valid flag
-		end
-		
-		else 
-			r_cycle_ctr <= r_cycle_ctr + 1; //If we aren't at the desired counter value, increment the counter
 	end
 	
 endmodule
