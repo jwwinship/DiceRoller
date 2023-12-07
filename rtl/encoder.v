dda411edfdde13018f064c3195979673eef4e817
Winship <jwwinship@arc-schaumont-class-vm.wpi.edu>
2023-12-06 20:22:40 -0500

    Initial changes to encoder.v

--- rtl/encoder.v
+++ rtl/encoder.v
@@ -9,17 +9,15 @@ output dieSelect
 
 
 reg [6:0] comboButton;
-reg [2:0] dieSelect;
+wire [2:0] dieSelect;
 
-assign combobutton = {buttonD4 + buttonD8 + buttonD10 + buttonD12 + buttonD20 + switchTest};
+assign combobutton = {buttonD4, buttonD6, buttonD8,buttonD10,buttonD12,buttonD20,switchTest};
 
 
 
 
 always @ (comboButton) begin
-
 	case (combobutton) begin
-
 
 	7'b1000000: dieSelect = 3'b000;
 	7'b0100000: dieSelect = 3'b001;
